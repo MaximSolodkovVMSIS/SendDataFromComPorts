ee2a579bea4c361b488e40ccd6ed3778f947c6bc
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-09-28 17:40:51 +0300

    add packet transmission and byte-staffing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -64,6 +64,43 @@ void MainWindow::populateParityOptions(QComboBox *comboBoxParity) {
     comboBoxParity->addItem("Space Parity", QSerialPort::SpaceParity);
 }
 
+QByteArray applyByteStuffing(const QByteArray& data) {
+    QByteArray stuffedData;
+    char ESC = 0x1B; // Символ для экранирования
+    char FLAG = '$'; // Флаг начала пакета
+
+    for (char byte : data) {
+        if (byte == ESC || byte == FLAG) {
+            stuffedData.append(ESC);  // Добавляем ESC перед управляющими символами
+        }
+        stuffedData.append(byte);  // Добавляем сам байт
+    }
+
+    return stuffedData;
+}
+
+QByteArray removeByteStuffing(const QByteArray& stuffedData, QList<int>& modifiedIndices) {
+    QByteArray unstuffedData;
+    char ESC = 0x1B;  // Предположим, что символ экранирования - 0x1B
+    bool isEscaped = false;
+
+    for (int i = 0; i < stuffedData.size(); ++i) {
+        char byte = stuffedData[i];
+
+        if (isEscaped) {
+            modifiedIndices.append(i - 1);  // Сохраняем индекс экранирующего байта
+            unstuffedData.append(byte);  // Добавляем экранированный байт
+            isEscaped = false;  // Сбрасываем флаг экранирования
+        } else if (byte == ESC) {
+            isEscaped = true;  // Устанавливаем флаг экранирования
+        } else {
+            unstuffedData.append(byte);  // Добавляем обычный байт
+        }
+    }
+
+    return unstuffedData;
+}
+
 void MainWindow::configurePort1() {
     QString portName = ui->comboBoxComPort1->currentText();
     serialPort1->setPortName(portName);
@@ -89,26 +126,64 @@ void MainWindow::configurePort2() {
 
 void MainWindow::readDataFromPort1() {
     if (serialPort1->isOpen()) {
-        QByteArray data = serialPort1->readAll();
-        if (!data.isEmpty()) {
+        QByteArray stuffedData = serialPort1->readAll();
+
+        if (!stuffedData.isEmpty()) {
             ui->textEditOutput1->clear();
-            ui->textEditOutput1->insertPlainText(QString::fromUtf8(data));
+
+            QList<int> modifiedIndices;
+            QByteArray unstuffedData = removeByteStuffing(stuffedData, modifiedIndices);
+
+            QString frameInfo = "Принятый кадр (до дебайт-стаффинга):\n";
+            for (int i = 0; i < stuffedData.size(); ++i) {
+                if (modifiedIndices.contains(i)) {
+                    frameInfo += QString("<b>%1</b> ").arg(QString::number(static_cast<unsigned char>(stuffedData[i]), 16).rightJustified(2, '0'));
+                } else {
+                    frameInfo += QString("%1 ").arg(QString::number(static_cast<unsigned char>(stuffedData[i]), 16).rightJustified(2, '0'));
+                }
+            }
+
+            ui->textEditOutput1->append(frameInfo);
+
+            QString unstuffedFrameInfo = QString("Кадр после дебайт-стаффинга: %1").arg(QString(unstuffedData));
+            ui->textEditOutput1->append(unstuffedFrameInfo);
         }
     }
 }
+
+
 
 void MainWindow::readDataFromPort2() {
     if (serialPort2->isOpen()) {
-        QByteArray data = serialPort2->readAll();
-        if (!data.isEmpty()) {
-            ui->textEditOutput2->clear();
-            ui->textEditOutput2->insertPlainText(QString::fromUtf8(data));
+        QByteArray stuffedData = serialPort2->readAll();
+
+        if (!stuffedData.isEmpty()) {
+            ui->textEditOutput1->clear();
+
+            QList<int> modifiedIndices;
+            QByteArray unstuffedData = removeByteStuffing(stuffedData, modifiedIndices);
+
+            QString frameInfo = "Принятый кадр (до дебайт-стаффинга):\n";
+            for (int i = 0; i < stuffedData.size(); ++i) {
+                if (modifiedIndices.contains(i)) {
+                    frameInfo += QString("<b>%1</b> ").arg(QString::number(static_cast<unsigned char>(stuffedData[i]), 16).rightJustified(2, '0'));
+                } else {
+                    frameInfo += QString("%1 ").arg(QString::number(static_cast<unsigned char>(stuffedData[i]), 16).rightJustified(2, '0'));
+                }
+            }
+
+            ui->textEditOutput2->append(frameInfo);
+
+            QString unstuffedFrameInfo = QString("Кадр после дебайт-стаффинга: %1").arg(QString(unstuffedData));
+            ui->textEditOutput2->append(unstuffedFrameInfo);
         }
     }
 }
 
+
+
 void MainWindow::sendDataFromPort1() {
-    ui->textEdit->clear();
+    ui->textEdit2->clear();
     ui->textEditOutput2->clear();
 
     configurePort1();
@@ -116,23 +191,63 @@ void MainWindow::sendDataFromPort1() {
 
     QString text = ui->textEditInput1->toPlainText();
     QByteArray data = text.toUtf8();
-    int byteCount = data.size();
 
-    // Отправка данных
+    QByteArray stuffedData = applyByteStuffing(data);
+
+    char groupName = 3;
+    QByteArray flag = QByteArray::fromStdString("$" + std::string(1, 'a' + groupName));
+    QByteArray destinationAddress(2, 0);
+    QByteArray sourceAddress = QByteArray::number(1);
+    QByteArray otherFields(2, 0);
+    QByteArray fcs(2, 0);
+
+    QByteArray packet;
+    packet.append(flag);
+    packet.append(destinationAddress);
+    packet.append(sourceAddress);
+    packet.append(otherFields);
+    packet.append(stuffedData);
+    packet.append(fcs);
+
+    int byteCount = packet.size();
+
     if (serialPort1->isOpen()) {
-        serialPort1->write(data);
+        serialPort1->write(packet);
         serialPort1->waitForBytesWritten(10);
     }
 
-    QString info = QString("From port %1 to port %2 send:\nString: \"%3\"(bytes: %4) Speed: 9600 Parity: %5\n")
-                       .arg(ui->comboBoxComPort1->currentText())
-                       .arg(ui->comboBoxComPort2->currentText())
-                       .arg(text)
-                       .arg(byteCount)
-                       .arg(parityToString(serialPort1->parity()));
+    QString byteInfoBefore = QString("Данные до байт-стаффинга: \"%1\"\n"
+                                     "Байты: %2")
+                                 .arg(QString(data))
+                                 .arg(QString(data.toHex(' ')));
 
-    ui->textEdit->append(info);
+    QString byteInfoAfter = QString("Данные после байт-стаффинга: \"%1\"\n"
+                                    "Байты: %2")
+                                .arg(QString(stuffedData))
+                                .arg(QString(stuffedData.toHex(' ')));
+
+    QString packetInfo = QString("Пакет для отправки:\n"
+                                 "Флаг: \"%1\"\n"
+                                 "Адрес назначения: %2\n"
+                                 "Адрес источника: %3\n"
+                                 "Прочие поля: %4\n"
+                                 "Данные: \"%5\"\n"
+                                 "FCS: %6\n"
+                                 "Общий размер пакета: %7 байт")
+                             .arg(QString(flag))
+                             .arg(QString(destinationAddress.toHex(' ')))
+                             .arg(QString(sourceAddress.toHex(' ')))
+                             .arg(QString(otherFields.toHex(' ')))
+                             .arg(QString(stuffedData.toHex(' ')))
+                             .arg(QString(fcs.toHex(' ')))
+                             .arg(byteCount);
+
+    ui->textEdit2->append(byteInfoBefore);
+    ui->textEdit2->append(byteInfoAfter);
+    ui->textEdit2->append(packetInfo);
 }
+
+
 
 void MainWindow::sendDataFromPort2() {
     ui->textEdit->clear();
@@ -143,21 +258,48 @@ void MainWindow::sendDataFromPort2() {
 
     QString text = ui->textEditInput2->toPlainText();
     QByteArray data = text.toUtf8();
-    int byteCount = data.size();
 
+    char groupName = 3;
+    QByteArray flag = QByteArray::fromStdString("$" + std::string(1, 'a' + groupName));
+    QByteArray destinationAddress(2, 0);
+    QByteArray sourceAddress = QByteArray::number(1);
+    QByteArray otherFields(2, 0);
+    QByteArray fcs(2, 0);
+
+    QByteArray packet;
+    packet.append(flag);
+    packet.append(destinationAddress);
+    packet.append(sourceAddress);
+    packet.append(otherFields);
+    packet.append(data);
+    packet.append(fcs);
+
+    int byteCount = packet.size();
+
+    // Отправка данных
     if (serialPort2->isOpen()) {
         serialPort2->write(data);
         serialPort2->waitForBytesWritten(10);
     }
 
-    QString info = QString("From port %1 to port %2 send:\nString: \"%3\"(bytes: %4) Speed: 9600 Parity: %5\n")
-                       .arg(ui->comboBoxComPort2->currentText())
-                       .arg(ui->comboBoxComPort1->currentText())
-                       .arg(text)
-                       .arg(byteCount)
-                       .arg(parityToString(serialPort2->parity()));
+    QString packetInfo = QString("Пакет для отправки:\n"
+                                 "Флаг: \"%1\"\n"
+                                 "Адрес назначения: %2\n"
+                                 "Адрес источника: %3\n"
+                                 "Прочие поля: %4\n"
+                                 "Данные: \"%5\" (в байтах: %6)\n"
+                                 "FCS: %7\n"
+                                 "Общий размер пакета: %8 байт")
+                             .arg(QString(flag))
+                             .arg(QString(destinationAddress.toHex(' ')))  // Выводим адрес назначения в виде hex
+                             .arg(QString(sourceAddress.toHex(' ')))  // Адрес источника в hex
+                             .arg(QString(otherFields.toHex(' ')))  // Прочие поля
+                             .arg(QString(data))  // Текстовые данные
+                             .arg(data.size())  // Размер данных
+                             .arg(QString(fcs.toHex(' ')))  // FCS в hex
+                             .arg(byteCount);  // Общий размер пакета
 
-    ui->textEdit->append(info);
+    ui->textEdit->append(packetInfo);
 }
 
 
